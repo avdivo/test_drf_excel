2ca9aa13fb541c1f5b484459fc40cd769da907d4
alex <avdivo@mail,ru>
2022-07-05 02:17:05 +0300

    Next

--- test_drf_excel/urls.py
+++ test_drf_excel/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+from django.urls import include, path
+from rest_framework import routers
+from myapi import views
+
+router = routers.DefaultRouter()
+router.register(r'file', views.FileUploadViewSet, basename='file')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+	path('upload/', include(router.urls)),
 ]
