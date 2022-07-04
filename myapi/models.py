d5264f910ebd9c0c4af9c39dea9618a2b863b8f2
alex <avdivo@mail,ru>
2022-07-05 02:17:59 +0300

    Next

---
+++ myapi/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+# Клиенты
+class Client(models.Model):
+    name = models.CharField(max_length=32, blank=False, unique=True, verbose_name=u'Клиент')  # Название клиента
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+# Организации
+class Organization(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)  # Связь с таблицей Клиенты
+    name = models.CharField(max_length=32, blank=False, unique=True, verbose_name=u'Организация')
+    address = models.CharField(max_length=256, verbose_name=u'Адрес организации')
+    fraud_weight = models.IntegerField(blank=False, default=0, verbose_name=u'Мошенничество')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
+
+
+# Счета организаций
+class Bill(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)  # Связь с таблицей Клиенты
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)  # Связь с таблицей Организации
+    number = models.CharField(max_length=32, verbose_name=u'Номер счета')
+    sum = models.IntegerField(default=0, verbose_name=u'Сумма')
+    date = models.DateTimeField(blank=True, verbose_name=u'Дата создания')
+    service_class = models.IntegerField(blank=True, verbose_name=u'Номер')
+    service_name = models.CharField(max_length=64, blank=True, verbose_name=u'Услуга')
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = 'Счет'
+        verbose_name_plural = 'Счета'
---
+++ myapi/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+
+from .serializers import FileSerializer
+
+
+class FileUploadViewSet(viewsets.ViewSet):
+
+    def create(self, request):
+        serializer_class = FileSerializer(data=request.data)
+        if 'file' not in request.FILES or not serializer_class.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            handle_uploaded_file(request.FILES['file'])
+            return Response(status=status.HTTP_201_CREATED)
+
+def handle_uploaded_file(f):
+    with open(f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
